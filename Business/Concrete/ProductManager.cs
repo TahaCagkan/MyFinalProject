05b41b549c3cb43f91a0469fe754531aca953dbb
CAGKAN <tahacagkan@gmail.com>
2021-02-18 00:30:10 +0300

    17.02.2021 Validation Eklendi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Resutls;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,14 +24,9 @@ namespace Business.Concrete
             _productDal = productDal;
         }
         //Ekleme 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if(product.ProductName.Length<2)
-            {
-                //harf 2 karakterden büyük olamaz
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
-
             _productDal.Add(product);
             //eklendiği zaman mesaj eklenicek
             return new SuccessResult(Messages.ProductAdded);
